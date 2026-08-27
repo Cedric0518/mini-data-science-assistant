fd29d6a960e1ab356bcdca29e05d17d7cd1f69a0
KESSAHOU Cedric <Ulrikess@users.noreply.huggingface.co>
2026-08-27 04:25:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ with gr.Blocks(title="Mini Data Science Assistant") as demo:
 
     file = gr.File(
         label="Upload your CSV",
-        file_types=[".csv"]
+        file_types=[".csv"],
         type="filepath"
     )
 
